9148beb066e8dcdfc3612a4e321817c66085b497
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-07-23 23:58:23 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,27 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+function randomCalc(leng) {
+  return Math.floor(Math.random() * leng);
+}
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+};
+document.getElementById("startworker").onclick = function() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework", " my phone", " the car"];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+  var r =
+    who[randomCalc(who.length)] +
+    action[randomCalc(action.length)] +
+    what[randomCalc(what.length)] +
+    when[randomCalc(when.length)];
+  document.getElementById("result").innerText = r;
 };
